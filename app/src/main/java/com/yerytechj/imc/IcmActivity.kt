9040449c75aa235b2ddfbf720baf88cb4b4bef41
yerytech <202000854@p.uapa.edu.do>
2024-05-20 20:36:05 -0700

    IMC Android Native

---
+++ app/src/main/java/com/yerytechj/imc/IcmActivity.kt
@@ -0,0 +1,23 @@
+package com.yerytechj.imc
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class IcmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        supportActionBar?.hide()
+
+        setContentView(R.layout.activity_icm)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
