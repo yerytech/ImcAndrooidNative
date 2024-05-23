587dedcba3ca51d30b13afa04193bf9e86e93841
yerytech <202000854@p.uapa.edu.do>
2024-05-22 18:48:42 -0700

    add property in activity

--- app/src/main/java/com/yerytechj/imc/IcmActivity.kt
+++ app/src/main/java/com/yerytechj/imc/IcmActivity.kt
@@ -1,13 +1,29 @@
 package com.yerytechj.imc
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.slider.RangeSlider
 
 
 class IcmActivity : AppCompatActivity() {
+    val viewMan: CardView = findViewById(R.id.viewMan)
+    val viewWoman: CardView = findViewById(R.id.viewWoman)
+    val tvHeight: TextView = findViewById(R.id.tvHeight)
+    val tvWeightNumber: TextView = findViewById(R.id.tvWeightNumber)
+    val tvAgeNumber: TextView = findViewById(R.id.tvAgeNumber)
+    val rsHeightSlider: RangeSlider = findViewById(R.id.rsHeightSlider)
+    val btnMinusWei:FloatingActionButton=findViewById(R.id.btnMinusWei)
+    val bntPlusWei:FloatingActionButton=findViewById(R.id.btnPlusWei)
+    val bntMinusAge:FloatingActionButton=findViewById(R.id.btnMinusAge)
+    val btnPlusAge:FloatingActionButton=findViewById(R.id.btnPlusAge)
+    val btnCalculate:Button=findViewById(R.id.btnCalculate)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,6 +34,8 @@ class IcmActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
         }
     }
 }
